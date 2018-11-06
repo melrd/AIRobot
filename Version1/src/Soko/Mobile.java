93130727fc41ef3958fb644aaf95e0d0de1fc01f
lisejly <jolly.lise@gmail.com>
2018-11-06 12:57:36 +0100

    code IA

---
+++ Version1/src/Soko/Mobile.java
@@ -0,0 +1,52 @@
+package Soko;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Mobile extends Element {
+
+    /**
+     * Default constructor
+     */
+    public Mobile() {
+    }
+
+    /**
+     * 
+     */
+    public Configuration config;
+
+    /**
+     * 
+     */
+    public Position position;
+
+    /**
+     * @param Type 
+     * @param Configuration 
+     * @param Position
+     */
+    public void Mobile(Type type, Configuration config, Position position) {
+        // TODO implement here
+    }
+
+    /**
+     * @param Position 
+     * @return
+     */
+    public boolean setPosition(Position position) {
+        // TODO implement here
+        return false;
+    }
+
+    /**
+     * @return
+     */
+    public Position getPosition() {
+        // TODO implement here
+        return null;
+    }
+
+}
