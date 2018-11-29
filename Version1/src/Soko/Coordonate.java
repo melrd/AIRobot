6b36cc48b17414866ac6c390115c818b22101a38
lisejly <jolly.lise@gmail.com>
2018-11-29 15:14:02 +0100

    update

---
+++ Version1/src/Soko/Coordonate.java
@@ -0,0 +1,46 @@
+package Soko;
+
+public class Coordonate {
+	int column, // number of the coordonate  for line and column
+		line;
+	boolean state; // state of our object or if you transport something or not
+	
+	public Coordonate() { // implements at null all value
+		column = 0;
+		line = 0;
+		state = false;
+	}
+	
+	public Coordonate(int pColumn, int pLine) { // implements the coordonate
+		column = pColumn;
+		line = pLine;
+		state = false;
+	}
+
+	//getter & setter for all variable of this class
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	public int getLine() {
+		return line;
+	}
+
+	public void setLine(int line) {
+		this.line = line;
+	}
+
+	public boolean isState() {
+		return state;
+	}
+
+	public void setState(boolean state) {
+		this.state = state;
+	}
+	
+
+}
