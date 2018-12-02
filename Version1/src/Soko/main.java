0f6c077b41ff38f98e99e64044b753568dca9d50
lisejly <jolly.lise@gmail.com>
2018-12-02 18:18:42 +0100

    Revert "pb"
    This reverts commit fcdaa72e165b2469ad5ed55ffd9db01922a62e3b.

--- Version1/src/Soko/main.java
+++ Version1/src/Soko/main.java
@@ -24,8 +24,7 @@ public class main {
 		char[][] mapInitiate = null, //map for the reading part
 				mapClean = null; // map without any diamonds or goal
 		int startColumn = 0,
-			startLine = 0,
-			numberGoal = 0;
+			startLine = 0;
 		Tree tree = null; // decision tree
 		Fipo fifo = new Fipo(); // file for run the tree
 		boolean endOfTree = false;
@@ -66,21 +65,15 @@ public class main {
 		
 		System.out.println("\n");
 		
-		
-		// run the fifo with a end point
-		/**while (fifo.fifo.get(0).steps < 6) {
-			// for the first node of fifo we check the movment possible for it
-			checkAround(mapInitiate ,fifo.fifo.get(0), fifo);
-			if(fifo.fifo == null)
-				break;
-		}*/
 		Node last = new Node();
 		
 		while(fifo.fifo.size() > 0 && endOfTree == false) {
 			// besoin du dernier des noeuds
 			Graph graph = new Graph();
+			System.out.println("Node : ");
 			printNode(fifo.fifo.get(0));
 			endOfTree = calculation(fifo, mapClean, graph);
+			fifo.fifo.remove(0);
 			last = graph.finalN;
 			try {
 	            Thread.sleep(100);
@@ -91,7 +84,7 @@ public class main {
 		
 		
 		System.out.println("\n");
-		System.out.println("Way");
+		System.out.println("Final Way");
 		//return the way for get it
 		temp = fifo.fifo.get(0).listNode(last, temp);
 		System.out.println(temp.size());
@@ -111,7 +104,7 @@ public class main {
 			numberLine = 0,
 			numberGoal = 0;
 		try{
-		    File f = new File ("C:\\Users\\jolly\\Documents\\GitHub\\AIRobot\\Version1\\scheme1.txt");
+		    File f = new File ("C:\\Users\\Asus\\Documents\\GitHub\\AIRobot\\Version1\\scheme1.txt");
 		    FileReader fr = new FileReader (f);
 		    BufferedReader br = new BufferedReader (fr);
 		 
@@ -152,53 +145,6 @@ public class main {
 		return map;
 	}           
 
-	private static void checkAround (char[][] map, Node node, Fipo fifo) {
-		node.printNode();
-		
-		/** for the node send
-		 * check each direction one by one
-		 * we start by looking if we can move in this direction
-		 * if we can, we check we don t already pass on it before (round trip)
-		 * if we don t, we add this child node and we put this node in the file.
-		 * then we print the node
-		 */
-/**		if (node.GameRules(map,DIRECTION.DOWN) == true) {
-//			System.out.println("-- test D --");
-			if(node.roundTrip( DIRECTION.DOWN) == true) {
-				node.addNode(node,DIRECTION.DOWN);
-				fifo.nodeCheck(node, node.down);
-			}
-		}
-//		printNode(node.down);
-		
-		if (node.GameRules(map,DIRECTION.LEFT) == true) {
-//			System.out.println("-- test L --");
-			if(node.roundTrip(DIRECTION.LEFT) == true) {
-				node.addNode(node,DIRECTION.LEFT);
-				fifo.nodeCheck(node, node.left);
-			}
-		}
-//		printNode(node.left);
-		
-		if (node.GameRules(map,DIRECTION.UP) == true) {
-//			System.out.println("-- test U --");
-			if(node.roundTrip(DIRECTION.UP) == true) {
-				node.addNode(node,DIRECTION.UP);
-				fifo.nodeCheck(node, node.up);
-			}
-		}
-//		printNode(node.up);
-		
-		if (node.GameRules(map,DIRECTION.RIGHT) == true) {
-//			System.out.println("-- test R --");
-			if(node.roundTrip(DIRECTION.RIGHT) == true) {
-				node.addNode(node,DIRECTION.RIGHT);
-				fifo.nodeCheck(node, node.right);
-			}
-		}
-		//printNode(node.right);
-	*/}
-
 	// check if the node is not empty before tho call the function in the node class
 	private static void printNode(Node node) {
 		if(node != null)
@@ -214,21 +160,25 @@ public class main {
 			return false;
 		
 		Node node = fifo.fifo.get(0);
+		System.out.println("start point");
+		printNode(node);
+		System.out.println(node.tabGoal.get(0).state);
 		
 		if (node.coordinate.state == false) { // observe if we transport a diamond or not
 			for (Coordonate e : node.tabDiamond) { // we don t have any diamond so we are looking for one
 				if(e.state == false) { // looking for a diamond who we can move
 					//calcul du chemin retourne une liste de noeud
 					temp = graph.bestDistance(map, node, node.tabDiamond.get(node.tabDiamond.indexOf(e)));
-//					System.out.println("best distance");
-//					for (Node z : temp)
-//						printNode(z);
+					System.out.println("Way");
+					for (Node z : temp)
+						printNode(z);
 					// add the way in the tree and add the last node of the way ine the file
-					fifo.nodeCheck(node, copyWay(node, temp, 1));
-					System.out.println("Diamond \n\n");
-					//printNode(temp.get(temp.size()-1));
-			        try {
-			            Thread.sleep(100);
+					fifo.nodeCheck(node, node.copyShortWay(node, temp));
+					System.out.println("Diamond");
+					printNode(fifo.fifo.get(fifo.fifo.size()-1));
+					System.out.println("________ \n\n");
+					try {
+			            Thread.sleep(50);
 			        } catch (InterruptedException f) {
 			            f.printStackTrace();
 			        }
@@ -236,19 +186,30 @@ public class main {
 			}
 		}
 		else { // we have a diamond so we are looking for a goal
+			System.out.println("a");
 			for (Coordonate e : node.tabGoal) {
+				System.out.println("b");
+				System.out.println(e.state);
 				if(e.state == false) {
 					// calcul du chemin
+					System.out.println("c");
 					temp = graph.bestDistance(map, node, node.tabGoal.get(node.tabGoal.indexOf(e)));
+					System.out.println("Way");
+					for (Node z : temp)
+						printNode(z);
+					
 					for (Coordonate f : node.tabDiamond) {
 						if(f.line == temp.get(0).coordinate.line && f.column == temp.get(0).coordinate.column)
+							System.out.println("[" + f.line + "][" + f.column);
 							positionDiamond = node.tabDiamond.indexOf(f);
 					}
 					// add the way in the tree and add the last node of the way int the file
-					fifo.nodeCheck(node, copyWay(node, temp, 1, positionDiamond, node.tabGoal.indexOf(e)));
-					System.out.println("Goal \n\n");
+					fifo.nodeCheck(node, node.copyShortWay(node, temp, positionDiamond, node.tabGoal.indexOf(e)));
+					System.out.println("Goal");
+					printNode(fifo.fifo.get(fifo.fifo.size()-1));
+					System.out.println("________ \n\n");
 			        try {
-			            Thread.sleep(100);
+			            Thread.sleep(50);
 			        } catch (InterruptedException f) {
 			            f.printStackTrace();
 			        }
@@ -263,134 +224,6 @@ public class main {
 		if(node.checkEnd())
 			return true;
 		else return false;
-	}
-
-	//copy shortest way in the treee
-	private static Node copyWay(Node node, ArrayList<Node> e, int position) {
-		/**
-		 * recursive function will check each node of the tab
-		 * try to find the direction of the way
-		 * if the node is not add, we create it
-		 * we check if we are at the end of the new way or not
-		 * return the last node
-		 */
-	
-		if(node == e.get(e.size()-1))
-			return node;
-		
-		else if(e.get(position).coordinate.column == node.coordinate.column) {
-			if(e.get(position).coordinate.line == node.coordinate.line -1) {
-				if(node.down == null) 
-					node.down = new Node(e.get(position).coordinate.column, e.get(position).coordinate.line, node);
-				if (position ++ < e.size()) 
-					copyWay(node.down, e, position ++);
-				else 
-					return node.down;
-			}
-				
-			else if(e.get(position).coordinate.line == node.coordinate.line +1) {
-				if(node.up == null) 
-					node.up = new Node(e.get(position).coordinate.column, e.get(position).coordinate.line, node);					System.out.println("create node");}
-				if (position ++ < e.size()) 
-					copyWay(node.up, e, position ++);
-				else 
-					return node.up;
-			}
-			
-		else if(e.get(position).coordinate.line == node.coordinate.line) {
-			if(e.get(position).coordinate.column == node.coordinate.column -1 ) { 
-				if(node.left == null) 
-					node.left = new Node(e.get(position).coordinate.column, e.get(position).coordinate.line, node);
-				if (position ++ < e.size()) 
-					copyWay(node.left, e, position ++);
-				else 
-					return node.left;
-			}
-			
-			else if(e.get(position).coordinate.column == node.coordinate.column +1) {
-				if(node.right == null)
-					node.right = new Node(e.get(position).coordinate.column, e.get(position).coordinate.line, node);
-				if (position ++ < e.size()) 
-					copyWay(node.right, e, position ++);
-				else 
-					return node.right;
-			}
-		}
-		return null;
-	}
-	
-	// copy shortest way for the diamonds in the tree, change the position of the diamond and change the state of the goal and the diamonds at the end
-	private static Node copyWay(Node node, ArrayList<Node> e, int position, int diamond, int goal) {
-		/**
-		 * recursive function will check each node of the tab
-		 * try to find the direction of the way
-		 * if the node is not add, we create it
-		 * we also change the position of the diamonds
-		 * we check if we are at the end of the new way or not
-		 * return the last node and change the state of the diamond and the goal
-		 */
-		
-		if(node == e.get(e.size()-1))
-			return node;
-		
-		if(e.get(position).coordinate.column == node.coordinate.column) {
-			
-			if(e.get(position).coordinate.line == node.coordinate.line -1) {
-				if(node.down == null)
-					node.down = new Node(e.get(position).coordinate.column, e.get(position).coordinate.line, node);
-				node.down.tabDiamond.get(diamond).line --;
-				if (position ++ < e.size())
-					copyWay(node.down, e, position ++, diamond, goal);
-				else {
-					node.down.tabDiamond.get(diamond).state = true;
-					node.down.tabGoal.get(goal).state = true;
-					return node.down;
-				}
-			}
-				
-			else if(e.get(position).coordinate.line == node.coordinate.line +1) {
-				if(node.up == null)
-					node.up = new Node(e.get(position).coordinate.column, e.get(position).coordinate.line, node);
-				node.up.tabDiamond.get(diamond).line ++;
-				if (position ++ < e.size()) 
-					copyWay(node.up, e, position ++, diamond, goal);
-				else {
-					node.up.tabDiamond.get(diamond).state = true;
-					node.up.tabGoal.get(goal).state = true;
-					return node.up;
-				}
-			}
-		}
-			
-		else if(e.get(position).coordinate.line == node.coordinate.line) {
-			
-			if(e.get(position).coordinate.column == node.coordinate.column -1 ) { 
-				if(node.left == null) 
-					node.left = new Node(e.get(position).coordinate.column, e.get(position).coordinate.line, node);
-				node.left.tabDiamond.get(diamond).column--;
-				if (position ++ < e.size()) 
-					copyWay(node.left, e, position ++, diamond, goal);
-				else {
-					node.left.tabDiamond.get(diamond).state = true;
-					node.left.tabGoal.get(goal).state = true;
-					return node.left;
-				}
-			}
-			
-			else if(e.get(position).coordinate.column == node.coordinate.column +1) {
-				if(node.right == null) 
-					node.right = new Node(e.get(position).coordinate.column, e.get(position).coordinate.line, node);
-				node.right.tabDiamond.get(diamond).column++;
-				if (position ++ < e.size()) 
-					copyWay(node.right, e, position ++, diamond, goal);
-				else {
-					node.right.tabDiamond.get(diamond).state = true;
-					node.right.tabGoal.get(goal).state = true;
-					return node.right;
-				}
-			}
-		}
-		return null;
 	}
 	
 	private static ArrayList<Coordonate> createCoordonate (char[][]map, char type) { //create table of coordonate
